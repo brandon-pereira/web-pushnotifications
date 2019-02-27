e475e0b01bcb8a12e3f7a411768009a47309acdd
Brandon Pereira <brandonp@criticalmass.com>
2019-02-26 23:08:02 -0700

    mongo adapter

---
+++ src/adapters/mongo.js
@@ -0,0 +1,33 @@
+class Mongo {
+  constructor(mongoose) {
+    this.mongoose = mongoose;
+    this.collection = mongoose.model("NotificationQueue", {
+      userId: String,
+      date: Date,
+      payload: Object
+    });
+  }
+
+  async scheduleNotification(date, userId, payload) {
+    const insert = await this.collection.create({
+      userId,
+      date,
+      payload
+    });
+    return insert._id;
+  }
+
+  async fetchNotifications(date) {
+    return await this.collection.find({
+      date: {
+        $lt: date
+      }
+    });
+  }
+
+  async clearNotification({ id }) {
+    return await this.collection.deleteOne({ _id: id });
+  }
+}
+
+module.exports = Mongo;
